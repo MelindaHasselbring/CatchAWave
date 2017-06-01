4eaa28a0808ca3821cde69633c4a225bad429a08
Chad Cook <chadlcook3@gmail.com>
2017-05-31 21:07:59 -0700

    update5

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,71 +1,72 @@
-
-      // This example requires the Places library. Include the libraries=places
-      // parameter when you first load the API. For example:
-      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-     
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
 
-     $("#button").click(function(){
-    console.log("The paragraph was clicked.");
+
+$(document).ready(function() {
+  console.log();
+
+
+  $("#button").click(function() {
+
+    $(".map").show();
+
+    console.log("button clicked yo");
+
+  });
+
 });
+  var map;
+  var infowindow;
 
+  function initMap() {
+    var pyrmont = {
+      lat: 32.8636,
+      lng: -117.2546
+    };
 
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: pyrmont,
+      zoom: 15
+    });
 
-      $("#button").click(function(){
-    $("#map").hide();
-});
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: pyrmont,
+      radius: 1000,
+      type: ['restaurant']
+    }, callback);
+  }
 
-
-      var map;
-      var infowindow;
-
-      function initMap() {
-        var pyrmont = {lat: 32.8636, lng: -117.2546};
-
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: pyrmont,
-          zoom: 15
-        });
-
-        infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-          location: pyrmont,
-          radius: 1000,
-          type: ['restaurant', 'bar']
-        }, callback);
+  function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
       }
+    }
 
-      function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-          }
-        }
 
-        
-      }
+  }
 
-      initMap;
-
-      function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(place.name);
-          infowindow.open(map, this);
-        });
-      }
-
-      $("#button").click(function(){
-    console.log("The paragraph was clicked.");
-});
-
-      $("#button").click(function(){
-    $("#map").hide();
-});
  
+
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
+  }
+
+  $("#button").click(function() {
+    console.log("The paragraph was clicked.");
+  });
+
+
