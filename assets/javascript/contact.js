077b17be537619db7b4e41f0a467ecb703bd7e61
Melinda Hasselbring <melinda.hasselbring@gmail.com>
2017-06-07 18:45:33 -0700

    contactFinal

---
+++ assets/javascript/contact.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/4.1.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCaUM9_aPbThemfQkT-JxMtuTlWB1uVAJc",
+    authDomain: "catch-a-wave-fe8de.firebaseapp.com",
+    databaseURL: "https://catch-a-wave-fe8de.firebaseio.com",
+    projectId: "catch-a-wave-fe8de",
+    storageBucket: "catch-a-wave-fe8de.appspot.com",
+    messagingSenderId: "719812529097"
+  };
+  firebase.initializeApp(config);
+</script>
